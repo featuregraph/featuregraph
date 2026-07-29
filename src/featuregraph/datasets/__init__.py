fd9ea8c98158bd9a0658dbca1e9b7b04238a5220
featuregraph <nazia.habib@featuregraph.ai>
2026-07-29 14:04:46 -0500

    Export tank filling dataset

--- src/featuregraph/datasets/__init__.py
+++ src/featuregraph/datasets/__init__.py
@@ -5,9 +5,11 @@ FeatureGraph example datasets.
 from ._bidmc import bidmc
 from ._eastman import eastman
 from ._pulse import pulse
+from ._tank import tank_fill
 
 __all__ = [
     "bidmc",
     "eastman",
     "pulse",
+    "tank_fill",
 ]
