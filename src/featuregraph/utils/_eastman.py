41c62125d912600b1d57e21218bd6b8c05155feb
featuregraph <nazia.habib@featuregraph.ai>
2026-07-31 12:46:45 -0500

    style: normalize dataset loader imports

--- src/featuregraph/utils/_eastman.py
+++ src/featuregraph/utils/_eastman.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import pandas as pd
 import requests
 
-
 GITHUB_OWNER = "mv-per"
 GITHUB_REPOSITORY = "tennessee-eastman-dataset"
 GITHUB_REVISION = "309b944f35ac440ff0c70616947ffe723c766e14"
